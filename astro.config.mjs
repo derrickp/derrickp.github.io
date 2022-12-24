9613b60a3f5e2fef5f95b5c1277304c68e7410f2
Derrick Plotsky <derrickplotsky@gmail.com>
2022-12-24 09:46:20 -0800

    Set site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://derrickp.ca',
 	integrations: [mdx(), sitemap()],
 	server: { port: 8000 }
 });
