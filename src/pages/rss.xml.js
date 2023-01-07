0a0160db4bbf4d6f3ca9f7f5815f8f1c74e2fbc8
Derrick Plotsky <derrickplotsky@gmail.com>
2023-01-06 20:49:55 -0800

    Sort items in RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,26 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+
+const postImportResult = import.meta.glob("./blog/**/*.{md,mdx}", {
+  eager: true,
+});
+const items = Object.values(postImportResult)
+  .sort(
+    (a, b) =>
+      new Date(b.frontmatter.pubDate).valueOf() -
+      new Date(a.frontmatter.pubDate).valueOf()
+  )
+  .map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    description: post.frontmatter.description,
+    pubDate: post.frontmatter.pubDate,
+  }));
 
 export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
+  rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items,
+  });
